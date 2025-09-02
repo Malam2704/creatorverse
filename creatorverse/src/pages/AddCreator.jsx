897ede406461269f8f90c79088e9642cc2ce0cdb
Mohammed Alam <ma3655@rit.edu>
2025-09-02 02:33:35 -0400

    form to add creators

--- creatorverse/src/pages/AddCreator.jsx
+++ creatorverse/src/pages/AddCreator.jsx
@@ -1,3 +1,22 @@
+import { useState } from 'react';
+import { Box, TextField, Button, Typography, Paper } from '@mui/material';
+
 export default function AddCreator() {
-    return <div>Add Creator Page</div>;
+    return (
+        <Paper sx={{ maxWidth: 400, mx: 'auto', mt: 4, p: 3 }}>
+            <Typography variant="h5" align="center">
+                Add New Creator
+            </Typography>
+
+            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+                <TextField name="name" label="Creator Name" />
+                <TextField name="url" label="Creator URL" type="url" />
+                <TextField name="imageURL" label="Image URL" type="url" />
+                <TextField name="description" label="Description" />
+                <Button variant="contained" size="large" sx={{ mt: 1 }}>
+                    Add Creator
+                </Button>
+            </Box>
+        </Paper>
+    );
 }
